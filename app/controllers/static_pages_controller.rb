8743a20f4414f79624c39b91bdf3fc3af84719d5
Sia <siakaramalegos@gmail.com>
2015-12-19 14:57:30 -0600

    Adds logic for different navbar

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,17 @@
 class StaticPagesController < ApplicationController
+  # TODO: change to real current_user helper method once have users
+  before_action :set_logged_in, except: [:home]
+
   def home
+    @logged_in = false
   end
 
   def timeline
+  end
+
+  private
+
+  def set_logged_in
+    @logged_in = true
   end
 end
