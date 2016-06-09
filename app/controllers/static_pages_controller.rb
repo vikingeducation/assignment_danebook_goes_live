4810ce56467f763ccc67595eb9954ade56e909fc
Stephen Mayeux <stephenmayeux@gmail.com>
2016-06-10 02:18:48 +0900

    add about and photos pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,4 +8,10 @@ class StaticPagesController < ApplicationController
 
   def friends
   end
+
+  def about
+  end
+
+  def photos
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get 'timeline', to: 'static_pages#timeline'
   get 'friends', to: 'static_pages#friends'
+  get 'about', to: 'static_pages#about'
+  get 'photos', to: 'static_pages#photos'
 end
