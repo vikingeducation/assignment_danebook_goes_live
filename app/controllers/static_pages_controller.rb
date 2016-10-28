478c041f9f1bbbdaf9244113a92c71140ade917a
Joe Bernardi <joebernardi@hotmail.com>
2016-10-27 20:27:52 -0500

    add photos

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -13,4 +13,8 @@ class StaticPagesController < ApplicationController
 
 	end
 
+	def photos
+
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
 
   get '/friends' => 'static_pages#friends'
 
+  get '/about' => 'static_pages#about'
+
+  get '/photos' => 'static_pages#photos'
+
 end
