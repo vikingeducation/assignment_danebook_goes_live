59cb010f36563fee657c47b15492ef710214e3a0
Ann Allan <ann.cristina.allan@gmail.com>
2017-05-17 16:12:48 -0400

    Add friends page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,4 +5,7 @@ class StaticPagesController < ApplicationController
 
   def timeline
   end
+
+  def friends
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   get '/timeline' => "static_pages#timeline"
+  get '/friends' => "static_pages#friends"
 end
