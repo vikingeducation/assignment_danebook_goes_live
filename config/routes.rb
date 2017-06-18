21f1072d0bcc3896ac8d77353104357bd8b2a4a9
Roy Chen <roychenlei@gmail.com>
2017-06-18 14:24:34 +0800

    completed 'Friends Page' feature

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   get '/timeline', to: 'static_pages#timeline', as: :timeline
+
+  get '/friends', to: 'static_pages#friends', as: :friends
 end
