8673e67d2c35b90b5f70505a513de096d3314695
Sean Kelly <shonkelli@gmail.com>
2017-10-15 12:27:08 -0400

    wired up nav bar

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,9 @@
 class StaticPagesController < ApplicationController
   def index
   end
-  def about
-    get '/about', 'static_pages#about'
-  end
+  def about;  end
+  def friends; end
+  def timeline; end
+  def pain; end
+  def edit_profile; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "static_pages#index"
-    
+  root "static_pages#timeline"
+  get '/friends', to: 'static_pages#friends'
+  get '/about', to: 'static_pages#about'
+  get '/friends', to: 'static_pages#photos'
+  get '/about_edit', to: 'static_pages#about_edit'
 end
